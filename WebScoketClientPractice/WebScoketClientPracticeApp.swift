dfaf6ac888144ba7f2ce70723b40839fee633731
Joanne0119 <liuchengen52033@gmail.com>
2024-03-23 22:15:04 +0800

    Initial Commit

---
+++ WebScoketClientPractice/WebScoketClientPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebScoketClientPracticeApp.swift
+//  WebScoketClientPractice
+//
+//  Created by 劉丞恩 on 2024/3/23.
+//
+
+import SwiftUI
+
+@main
+struct WebScoketClientPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
